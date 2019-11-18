10a1204b76352e0e2bdd6947a215b9068311790d
Stefano Ruiz <53628533+stefanoruiz@users.noreply.github.com>
2019-11-18 15:59:10 -0700

    Changes to the file.

--- app.py
+++ app.py
@@ -1,30 +1,13 @@
-# from flask import Flask, render_template, request,  url_for, redirect, jsonify
-
-import json
-import pandas as pd
+from flask import Flask, render_template, request,  url_for, redirect, jsonify
 
 
-# app = Flask(__name__)
-data = {
-    "title": "some text",
-    "self_post": "some text"
-}
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return pass
 
 
-with open("dummy.json", "w") as reddit:
-    json.dump(data, reddit)
- 
-
-# TODO Return with JSON using jsonify. 
-def request_data(x):
-    """Return: JSON file"""
-    json_data = [data]
-    df = pd.DataFrame.from_dict(json_data, orient='columns')
-    
-    return df.to_json(orient='split')
-
-example = request_data(reddit)
-print(example)
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
