0ee25ff266dff92d5b7e30ad1e0d90a7972de2f2
davidanagy <davidanagy@users.noreply.github.com>
2019-11-19 13:05:43 -0800

    changed to sfw db

--- POST_HERE/functions.py
+++ POST_HERE/functions.py
@@ -5,8 +5,8 @@ import pymongo
 def get_subreddit_info(array, code):
     """Function written by Matthew that gets subreddit information based on ID numbers"""
     client = pymongo.MongoClient(code)
-    db = client.sub2
-    data = [db.sub2.find({'sub_id':int(num)})[0] for num in array]
+    db = client.sfw_db
+    data = [db.sfw_db.find({'sub_id':int(num)})[0] for num in array]
     return(data)
 
 
